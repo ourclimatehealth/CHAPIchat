63f000cf0024a4201213b00060af5268ab7168ba
Vishnu Ravi <vishnu.ravi@gmail.com>
2024-04-06 15:15:27 -0400

    Move server files to separate repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 //
 // This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
 //
@@ -6,22 +8,35 @@
 // SPDX-License-Identifier: MIT
 //
 
-import Image from 'next/image'
 import { generateGreeting } from '@stanfordbdhg/example-package'
+import Chatbot, {
+  FloatingActionButtonTrigger,
+  InputBarTrigger,
+  ModalView,
+} from "mongodb-chatbot-ui";
 
 export default function Home() {
   const greeting = generateGreeting()
 
+    const suggestedPrompts = [
+    "Why should I use the MongoDB Chatbot Framework?",
+    "How does the framework use Atlas Vector Search?",
+    "Do you support using LLMs from OpenAI?",
+  ];
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col items-center">
-        <Image
-          src={`${process.env.basePath || ''}/stanfordbiodesign.png`}
-          alt="Stanford Biodesign Logo"
-          width={634}
-          height={235}
-        />
-        <h1 className="text-3xl text-center mt-4">{`${greeting.message} to the ${greeting.project}`}</h1>
+      <Chatbot darkMode={true} serverBaseUrl="http://localhost:3000/api/v1">
+        <>
+          <InputBarTrigger suggestedPrompts={suggestedPrompts} />
+          <FloatingActionButtonTrigger text="My MongoDB AI" />
+          <ModalView
+            initialMessageText="Welcome to MongoDB AI Assistant. What can I help you with?"
+            initialMessageSuggestedPrompts={suggestedPrompts}
+          />
+        </>
+      </Chatbot>
       </div>
     </div>
   )
--- ingest/build/mongodbChatbotFrameworkDataSource.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const mongoDbChatbotFrameworkDocsDataSourceConstructor: () => Promise<import("mongodb-rag-ingest/sources").DataSource>;
-//# sourceMappingURL=mongodbChatbotFrameworkDataSource.d.ts.map
