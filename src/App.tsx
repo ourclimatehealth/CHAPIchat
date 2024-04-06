c8b19c3504c78b2c99e4135169fa726662a12c43
Vishnu Ravi <vishnu.ravi@gmail.com>
2024-04-06 15:46:07 -0400

    Update web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-'use client';
-
-//
-// This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
-//
-// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
-//
-// SPDX-License-Identifier: MIT
-//
-
-import { generateGreeting } from '@stanfordbdhg/example-package'
+import './App.css'
 import Chatbot, {
   FloatingActionButtonTrigger,
   InputBarTrigger,
   ModalView,
 } from "mongodb-chatbot-ui";
 
-export default function Home() {
-  const greeting = generateGreeting()
-
-    const suggestedPrompts = [
+function App() {
+  const suggestedPrompts = [
     "Why should I use the MongoDB Chatbot Framework?",
     "How does the framework use Atlas Vector Search?",
     "Do you support using LLMs from OpenAI?",
   ];
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col items-center">
+    <>
+      <div>
       <Chatbot darkMode={true} serverBaseUrl="http://localhost:3000/api/v1">
         <>
           <InputBarTrigger suggestedPrompts={suggestedPrompts} />
@@ -38,6 +26,8 @@ export default function Home() {
         </>
       </Chatbot>
       </div>
-    </div>
+    </>
   )
 }
+
+export default App
